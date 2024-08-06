fb5def181442c440c5ede11a2db3ba9be52d3527
Murphie Pettyjohn <98448438+Mothpy@users.noreply.github.com>
2024-08-05 18:43:08 -0700

    comment update

--- src/pastes/pastes.controller.js
+++ src/pastes/pastes.controller.js
@@ -6,18 +6,22 @@ const pastes = require("../data/pastes-data");
 // list of pastes 
 function list(req, res) {
   const { userId } = req.params;
+  // filter pastes by userId if provided, otherwise return all pastes 
   res.json({ data: pastes.filter(userId ? paste => paste.user_id == userId : () => true) });
 }
 
+// get the highest paste ID to ensure unique IDs for new pastes 
 let lastPasteId = pastes.reduce((maxId, paste) => Math.max(maxId, paste.id), 0)
 
-// check if paste has body data 
+// middleware to check if paste has a certain body property 
 function bodyDataHas(propertyName) {
   return function (req, res, next) {
     const { data = {} } = req.body;
+    // if property exists, proceed to the next middleware
     if (data[propertyName]) {
       return next();
     }
+    // if property does not exist, return 400 error
     next({
         status: 400,
         message: `Must include a ${propertyName}`
@@ -25,35 +29,40 @@ function bodyDataHas(propertyName) {
   };
 }
 
-// check if exposure property is valid 
+// middleware to check if 'exposure' property is valid 
 function exposurePropertyIsValid(req, res, next) {
   const { data: { exposure } = {} } = req.body;
   const validExposure = ["private", "public"];
+  // if valid, proceed to next middleware
   if (validExposure.includes(exposure)) {
     return next();
   }
+  // if invalid, return 400 error
   next({
     status: 400,
     message: `Value of the 'exposure' property must be one of ${validExposure}. Received: ${exposure}`,
   });
 }
 
-// check if syntax property is valid 
+// middleware to check if 'syntax' property is valid 
 function syntaxPropertyIsValid(req, res, next) {
   const { data: { syntax } = {} } = req.body;
   const validSyntax = ["None", "Javascript", "Python", "Ruby", "Perl", "C", "Scheme"];
+  // if valid, proceed to next middleware
   if (validSyntax.includes(syntax)) {
     return next();
   }
+  // if invalid, return 400 error
   next({
     status: 400,
     message: `Value of the 'syntax' property must be one of ${validSyntax}. Received: ${syntax}`,
   });
 }
 
-// check if expiration number is valid 
+// middleware to check if 'expiration' property is valid 
 function expirationIsValidNumber(req, res, next){
   const { data: { expiration }  = {} } = req.body;
+  // if expiration value is a Number, proceed to next middleware
   if (expiration <= 0 || !Number.isInteger(expiration)){
       return next({
           status: 400,
@@ -66,6 +75,7 @@ function expirationIsValidNumber(req, res, next){
 // create a new paste 
 function create(req, res) {
   const { data: { name, syntax, exposure, expiration, text } = {} } = req.body;
+  // create a new paste object with a unique ID
   const newPaste = {
     id: ++lastPasteId, // Increment last id then assign as the current ID
     name: name,
@@ -74,11 +84,12 @@ function create(req, res) {
     expiration: expiration,
     text: text,
   };
+  // add the new paste to the pastes array 
   pastes.push(newPaste);
   res.status(201).json({ data: newPaste });
 }
 
-// check if paste already exists 
+// middleware to check if paste already exists 
 function pasteExists(req, res, next) {
   const { pasteId } = req.params;
   const foundPaste = pastes.find((paste) => paste.id === Number(pasteId));
@@ -86,6 +97,7 @@ function pasteExists(req, res, next) {
     res.locals.paste = foundPaste;
     return next();
   }
+  // if paste doesn't exist, return 400 error
   next({
     status: 404,
     message: `Paste id not found: ${pasteId}`,
@@ -102,7 +114,7 @@ function update(req, res) {
   const paste = res.locals.paste;
   const { data: { name, syntax, expiration, exposure, text } = {} } = req.body;
 
-  // update the paste
+  // update the paste properties with the new values 
   foundPaste.name = name;
   foundPaste.syntax = syntax;
   foundPaste.expiration = expiration;
